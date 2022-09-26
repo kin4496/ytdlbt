dcf0fc30c21fe521360888198dc68d18e785ea5b
Insu Kang <kin4496@naver.com>
2022-09-26 19:58:57 +0900

    modify ytdlbt.py

--- ytdlbt/ytdlbt.py
+++ ytdlbt/ytdlbt.py
@@ -1,41 +1,70 @@
 from youtubesearchpython import *
-from re import match
-from typing import List
-import os
 from pytube import YouTube as yt
 
-def get_url(name,useOpt=False,kw=[],lim=10):
+import os
 
-    YOUTUBE_LINK_BASE="https://youtube.com{}"
+YOUTUBE_LINK_BASE="https://youtube.com{}"
 
-    kw = [kw_ for kw_ in kw if kw_ is not None]
+def search(title,artist,uploadDate=False,viewCount=False,rating=False):
 
-    if useOpt and len(kw):
-        name += '+' + '+'.join(kw)
+    if isinstance(artist,list):
+        if len(artist)!=0:
+            artist=''
+        else:
+            artist=artist[0]
     
-    # Replace all the spaces with +
-    name = name.replace(' ', '+')
-
-    results = CustomSearch(name,VideoSortOrder.viewCount ,limit=lim).result()["result"]
-    stripped_results = []
+    if artist == None or artist=='':
+        query=title
+    else:
+        query=f"{artist} {title}"
     
-    for video in results:
-        data = {}
-        data['title'] = video['title']
-        data['href'] = f"/watch?v={video['id']}"
-        data['author_name'] = video['channel']["name"]
-        data['duration'] = video['duration']
-        stripped_results.append(data)
+    if uploadDate:
+        filter=VideoSortOrder.uploadDate
+    elif viewCount:
+        filter=VideoSortOrder.viewCount
+    elif rating:
+        filter=VideoSortOrder.rating
+    else:
+        filter=None
     
-    return YOUTUBE_LINK_BASE.format(stripped_results[0]['href'])
+    if filter != None:
+        video=CustomSearch(query=query,searchPreferences=filter,limit=1).result()["result"]
+    else:
+        video=VideosSearch(query=query,limit=1).result()["result"]
 
-def _download(url,path='./'):
+    if video == None or len(video)==0:
+        print('음원을 찾지 못했습니다.')
+        return None
+
+    video=video[0]
+
+    url=f"/watch?v={video['id']}"
+    url=YOUTUBE_LINK_BASE.format(url)
+    
+    return url
+
+def download(url,path='./',progressive=None,adaptive=None,only_audio=True,file_extension=None):
+    
+    if url==None:
+        print('주소 값이 없습니다.')
+        return None
+
     music = yt(url)
-    music_stream = music.streams.filter(only_audio = True).first()
+    music_stream = music.streams.filter(progressive=progressive,adaptive=adaptive,
+                    only_audio = only_audio,file_extension=file_extension).first()
+    
+    if music_stream == None:
+        print('음악을 찾을 수 없습니다.')
+        return None
+
     music=music_stream.download(output_path=path)
     base , ext = os.path.splitext(music)
-    os.rename(music,base+'.mp3')
+    fname=base+'.mp3'
+    os.rename(music,fname)
 
-def download(name,path='./'):
-    url=get_url(name)
-    _download(url,path)
+    return fname
+
+
+if __name__=="__main__":
+    url=search('좋은날','아이유')
+    download(url)
